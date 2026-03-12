9ae9eea251a838f27fe15070a579a96c573d7b3a
Augusto Oliveira <gutocesar.c1@gmail.com>
2026-03-11 21:16:07 -0300

    feat: adicionado nome das colunas e configurado o banco H2

--- src/main/java/com/projeto/cadastro/Ninjas/NinjaModel.java
+++ src/main/java/com/projeto/cadastro/Ninjas/NinjaModel.java
@@ -18,16 +18,25 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY  )
-    private Long id;
+    @Column (name = "id")
+    private long id;
 
+    @Column (name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column (name = "img_url")
+    private String imgUrl;
+
+    @Column (name = "rank")
+    private String rank;
+
+    @Column (name = "idade")
     private int idade;
 
     @ManyToOne
     @JoinColumn(name= "missoes_id") //foreign key
-    private MissoesModel missoesModel;
+    private MissoesModel missoes;
 }
